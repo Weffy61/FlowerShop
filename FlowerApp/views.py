7967155bec0dc47f512ee164d2e0efbe35517bef
Weffy61 <weffy@mail.ru>
2024-03-11 20:39:19 +0300

    Prepair base.html and load static

--- FlowerApp/views.py
+++ FlowerApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'FlowerApp/index.html')
+
+
+def order(request):
+    return render(request, 'FlowerApp/order.html')
